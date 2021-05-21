3b81225ee628d9203ab4f83e04ea458ea0a1fd01
Jacob Ochoa <jacob.ochoa@gmail.com>
2021-05-21 00:47:16 -0700

    add release logic

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+GOOS=darwin GOARCH=amd64  go build -o bin/flint_darwin_amd64 -ldflags "-s -w" main.go
+GOOS=darwin GOARCH=arm64  go build -o bin/flint_darwin_arm64 -ldflags "-s -w" main.go
+GOOS=linux GOARCH=amd64  go build -o bin/flint_linux_amd64 -ldflags "-s -w" main.go
+GOOS=linux GOARCH=arm64  go build -o bin/flint_linux_arm64 -ldflags "-s -w" main.go
