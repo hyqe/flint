abd21d25032d921ae98d2cbd3c51a9e83ce1066b
Jacob Ochoa <jacob.ochoa@gmail.com>
2021-05-21 12:11:36 -0700

    fix push

--- push.sh
+++ push.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-
-version=$(cat VERSION)
-
-docker buildx create --name builder --use
-
-docker buildx inspect --bootstrap
-
-docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/arm/v7 \
-    -t hyqe/flint:$version username/demo:latest \
-    .
+image_latest="hyqe/flint:latest"
+image_version="hyqe/flint:$(cat VERSION)"
+platforms="linux/amd64,linux/arm64,linux/arm/v7"
 
 docker login
-
-docker push hyqe/flint:$version
-docker push hyqe/flint:latest
+docker buildx build --platform $platforms -t $image_latest -t $image_version --push .
